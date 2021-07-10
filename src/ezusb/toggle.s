a06777b5b0d1c54d91b03144f1b250080351c7cd
qitas <qitas@qitas.cn>
2021-07-10 20:38:38 +0800

    add demo src

---
+++ src/ezusb/toggle.s
@@ -0,0 +1,28 @@
+; toggles port A0
+
+.include "cypress.inc"
+
+.area DSEG (REL)
+
+.area CODE (ABS)
+; reset vector
+.org 0h0000
+
+; disable all interrupts
+clr EA
+
+; set clock speed
+mov dptr, #CPUCS
+movx a, @dptr
+;anl a, #~(1<<CPUCS.CLKSPD0 | 1<<CPUCS.CLKSPD1)		; 12 MHz (default)
+orl a, #(1<<CPUCS.CLKSPD0)				; 24 MHz
+movx @dptr, a
+
+; configure port A0 as an output
+orl OEA, #1
+
+loop:
+	; toggle port A0
+	clr IOA.0
+	setb IOA.0
+	sjmp loop
